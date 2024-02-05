3308299ceba9ae212c6dd7d64b413e3ba60e8cbc
Veselin Nikolov <147404281+zetnator@users.noreply.github.com>
2024-02-05 15:51:52 +0200

    add linq

--- CarStore_BL/Services/CarStoreService.cs
+++ CarStore_BL/Services/CarStoreService.cs
@@ -1,4 +1,5 @@
 ﻿using CarStore_BL.Interfaces;
+using CarStore_Models.Models;
 using CarStore_Models.Requests;
 using CarStore_Models.Responses;
 
@@ -15,13 +16,33 @@ namespace CarStore_BL.Services
             _carService = carService;
         }
 
+        public void AddCar(Car car)
+        {
+            throw new NotImplementedException();
+        }
+
         public int CheckBuyerCount(int input)
         {
             var carsCount = _carService.GetAllCars();
             return carsCount.Count + input;
         }
 
+        public void DeleteCar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAllCars()
+        {
+            throw new NotImplementedException();
+        }
+
         public GetCarsByBuyerIdResponse? GetByBuyer(GetCarsByBuyerIdRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car? GetCarById(int id)
         {
             throw new NotImplementedException();
         }
